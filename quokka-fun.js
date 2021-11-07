8961151007458c2c7a6e199df5266c390f0ecc40
Adam Aslan <chillcoders@gmail.com>
2021-11-06 20:30:54 -0400

    objectjs

--- quokka-fun.js
+++ quokka-fun.js
@@ -2,3 +2,50 @@ console.log ("helloworld");
 
 const a = 0;
 console.log (a);
+
+//https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/reduce
+
+const array1 = [1, 2, 3, 4];
+const reducer = (previousValue, currentValue) => previousValue + currentValue;
+
+// 1 + 2 + 3 + 4
+console.log(array1.reduce(reducer));
+const getMax = (a, b) => Math.max(a, b);
+
+// callback is invoked for each element in the array starting at index 0
+[1, 100].reduce(getMax, 50); // 100
+[    50].reduce(getMax, 10); // 50
+console.log(getMax);
+
+[0, 1, 2, 3, 4].reduce((previousValue, currentValue, currentIndex, array) => {
+    return previousValue + currentValue
+}, 10);
+
+let people = [
+    { name: 'Alice', age: 21 },
+    { name: 'Max', age: 20 },
+    { name: 'Jane', age: 20 }
+  ];
+  
+  function groupBy(objectArray, property) {
+    return objectArray.reduce(function (acc, obj) {
+      let key = obj[property]
+      if (!acc[key]) {
+        acc[key] = []
+      }
+      acc[key].push(obj)
+      return acc
+    }, {})
+  }
+  
+  let groupedPeople = groupBy(people, 'age')
+
+  console.log(groupedPeople);
+  // groupedPeople is:
+  // {
+  //   20: [
+  //     { name: 'Max', age: 20 },
+  //     { name: 'Jane', age: 20 }
+  //   ],
+  //   21: [{ name: 'Alice', age: 21 }]
+  // }
