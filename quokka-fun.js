94fb3b563bba4c8262868f2e56def0d9afc73cfa
Adam <chillcoders@gmail.com>
2021-11-09 12:12:06 -0500

    moreexamples

--- quokka-fun.js
+++ quokka-fun.js
@@ -1,7 +1,7 @@
-console.log ("helloworld");
+console.log("helloworld");
 
 const a = 0;
-console.log (a);
+console.log(a);
 
 //https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/reduce
 
@@ -14,38 +14,38 @@ const getMax = (a, b) => Math.max(a, b);
 
 // callback is invoked for each element in the array starting at index 0
 [1, 100].reduce(getMax, 50); // 100
-[    50].reduce(getMax, 10); // 50
+[50].reduce(getMax, 10); // 50
 console.log(getMax);
 
 [0, 1, 2, 3, 4].reduce((previousValue, currentValue, currentIndex, array) => {
-    return previousValue + currentValue
+  return previousValue + currentValue;
 }, 10);
 
 let people = [
-    { name: 'Alice', age: 21 },
-    { name: 'Max', age: 20 },
-    { name: 'Jane', age: 20 }
-  ];
-  
-  function groupBy(objectArray, property) {
-    return objectArray.reduce(function (acc, obj) {
-      let key = obj[property]
-      if (!acc[key]) {
-        acc[key] = []
-      }
-      acc[key].push(obj)
-      return acc
-    }, {})
-  }
-  
-  let groupedPeople = groupBy(people, 'age')
+  { name: "Alice", age: 21 },
+  { name: "Max", age: 20 },
+  { name: "Jane", age: 20 },
+];
 
-  console.log(groupedPeople);
-  // groupedPeople is:
-  // {
-  //   20: [
-  //     { name: 'Max', age: 20 },
-  //     { name: 'Jane', age: 20 }
-  //   ],
-  //   21: [{ name: 'Alice', age: 21 }]
-  // }
+function groupBy(objectArray, property) {
+  return objectArray.reduce(function (acc, obj) {
+    let key = obj[property];
+    if (!acc[key]) {
+      acc[key] = [];
+    }
+    acc[key].push(obj);
+    return acc;
+  }, {});
+}
+
+let groupedPeople = groupBy(people, "age");
+
+console.log(groupedPeople);
+// groupedPeople is:
+// {
+//   20: [
+//     { name: 'Max', age: 20 },
+//     { name: 'Jane', age: 20 }
+//   ],
+//   21: [{ name: 'Alice', age: 21 }]
+// }
