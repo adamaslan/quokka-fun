0eae96a9f4653a961a6c430689281c9062b91c25
Adam <chillcoders@gmail.com>
2022-01-15 23:59:21 -0500

    hi

--- reduce-more.js
+++ reduce-more.js
@@ -1,11 +1,16 @@
-const a = [2,3,4];
-let red = a.map (x => x + 1);
-console.log (red); 
+const a = [2, 3, 4];
+let red = a.map((x) => x + 1);
+console.log(red);
 
-let blue = a.reduce( function (x , y) {
-    if { y > 3}
-    return x} 100);
+let blue = a.reduce((x, y) => (y >= 3 ? y : null), []);
 
-console.log (blue);
+console.log(blue);
 
-
+const basket = [
+  { name: "apple", type: "fruit" },
+  { name: "broccoli", type: "vegetable" },
+];
+const fruits = basket.reduce(
+  (acc, food) => (food.type === "fruit" ? acc.concat(food) : acc),
+  []
+);
